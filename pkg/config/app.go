46d4dca97bd6d352781502e94ce79917bc02ee29
Quabena_the_dev <ebenezer.gyamfi@regent.edu.gh>
2024-12-28 15:08:01 +0000

    add workflow to go app

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -3,6 +3,7 @@ package config
 import (
     "fmt"
     "log"
+    "os"
     "time"
 
     "gorm.io/driver/mysql"
@@ -14,7 +15,16 @@ var Db *gorm.DB
 func Connect() {
     var db *gorm.DB
     var err error
-    dsn := "root:root@tcp(mysql:3306)/books?charset=utf8mb4&parseTime=True&loc=Local"
+
+    // Fetch environment variables
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    dbname := os.Getenv("DB_NAME")
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+        user, password, host, port, dbname)
 
     // Retry logic
     for i := 0; i < 5; i++ {
@@ -23,8 +33,7 @@ func Connect() {
             break
         }
         log.Printf("Database connection failed: %v. Retrying in 5 seconds...", err)
-        time.Sleep(10 * time.Second) // Initial delay before first connection attempt
-
+        time.Sleep(5 * time.Second)
     }
 
     if err != nil {
