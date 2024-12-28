d052c9654142d86c629a14b0b2149b16706f05d3
Quabena_the_dev <ebenezer.gyamfi@regent.edu.gh>
2024-12-28 18:11:09 +0000

    change health port to container name instead of localhost

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -8,17 +8,13 @@ import (
 
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
-    "github.com/joho/godotenv"
 )
 
 var Db *gorm.DB
 
 func Connect() {
-    // Load environment variables from .env file
-    err := godotenv.Load()
-    if err != nil {
-        log.Printf("Error loading .env file: %v", err)
-    }
+    var db *gorm.DB
+    var err error
 
     // Fetch environment variables
     user := os.Getenv("DB_USER")
@@ -27,13 +23,12 @@ func Connect() {
     port := os.Getenv("DB_PORT")
     dbname := os.Getenv("DB_NAME")
 
-    // Construct the DSN (Data Source Name)
     dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
         user, password, host, port, dbname)
 
-    // Retry logic for database connection
+    // Retry logic
     for i := 0; i < 5; i++ {
-        Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+        db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
         if err == nil {
             break
         }
@@ -45,5 +40,6 @@ func Connect() {
         log.Fatalf("Failed to connect to database after retries: %v", err)
     }
 
-    fmt.Println("Connected to the MySQL database.")
+    Db = db
+    fmt.Println("Connected to database.")
 }
